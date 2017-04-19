091ce8b2b6f5f76c1cddb92a4ea87c23c194d192
CP <cp.developer1111@gmail.com>
2017-04-19 08:03:34 -0400

    update configs and temporarily remove travis

--- src/directives.ts
+++ src/directives.ts
@@ -3,6 +3,6 @@ import { MultiPicker } from './components/multi-picker/multi-picker';
 export * from './components/multi-picker/multi-picker';
 export * from './components/multi-picker/multi-picker-options';
 
-export const MULTI_PICKER_DIRECTIVES = [
+export const SINGLE_LINE_FORM_DIRECTIVES = [
     MultiPicker
 ];
